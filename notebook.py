a2f9c41f26ff2d4c9fb802528e5431260939882d
parthkhanna150 <parth.khanna@mail.mcgill.ca>
2019-06-08 19:12:04 -0400

    first

---
+++ notebook.py
@@ -0,0 +1,19 @@
+
+#%%
+import pandas as pd
+import numpy as np
+
+
+#%%
+crimes = pd.read_csv("/Users/Khanna/Downloads/crime_cleaned_parth.csv")
+calls = pd.read_csv("/Users/Khanna/Downloads/calls_cleaned_parth.csv")
+
+
+#%%
+calls[:10]
+
+
+#%%
+
+
+
